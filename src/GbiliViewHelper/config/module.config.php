4e50d9c1a7ac917bfad6d5d41c894ef929e2906d
Guillermo Devi <g@gbili.com>
2015-02-01 14:15:29 +0100

    adapting to symlink module config keys

--- src/GbiliViewHelper/config/module.config.php
+++ src/GbiliViewHelper/config/module.config.php
@@ -2,10 +2,10 @@
 namespace GbiliViewHelper;
 
 return array(
-    //'navigation'         => include __DIR__ . '/navigation.config.php',
-    //'service_manager'    => include __DIR__ . '/service_manager.config.php',
     'view_helpers'       => include __DIR__ . '/view_helpers.config.php',
     'gbili_symlink_module' => array(
-        'gbili_view_helper_topnav_phtml' => __DIR__ . '/../view/partial/topnav.phtml',
+        'symlinks' => array(
+            'gbili_view_helper_topnav_phtml' => __DIR__ . '/../view/partial/topnav.phtml'
+        ),
     ),
 );
