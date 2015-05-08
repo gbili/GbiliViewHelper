967374b9b5d5b872a3a935b41791837f62b19fcb
Guillermo Devi <g@gbili.com>
2015-05-08 10:12:48 +0200

    webfont hack

--- src/GbiliViewHelper/config/module.config.php
+++ src/GbiliViewHelper/config/module.config.php
@@ -5,7 +5,8 @@ return array(
     'view_helpers'       => include __DIR__ . '/view_helpers.config.php',
     'gbili_symlink_module' => array(
         'symlinks' => array(
-            'gbili_view_helper_topnav_phtml' => __DIR__ . '/../view/partial/topnav.phtml'
+            'gbili_view_helper_topnav_phtml' => __DIR__ . '/../view/partial/topnav.phtml',
+            'gbili_webfont_js_phtml' => __DIR__ . '/../view/partial/webfont.js.phtml',
         ),
     ),
 );
